990d5504ee993e794ca14a1e802818e3ea637b0f
Rohan Kumar <annua1433@gmail.com>
2020-10-03 10:17:10 -0600

    basic homepage

--- mysite/todo/views.py
+++ mysite/todo/views.py
@@ -1,5 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render , redirect
+from django.contrib.auth.models import User
+from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
     return render(request, template_name = 'todo/index.html')
+def signup(request):
+    if request.method == 'POST':
+        username = request.POST.get('name')
+        firstname = request.POST.get('firstname')
+        lastname = request.POST.get('lastname')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+
+        try:
+            user = User.objects.create_user(username = username, first_name = firstname, last_name = lastname, email = email, password = password)
+        except:
+            return HttpResponse('user with these enteties already exist please enter different enteties')
+        else:
+            user.save()
+            return HttpResponse('user created')
+
+        
+    elif request.method == 'GET':
+        return redirect('todo:index')
